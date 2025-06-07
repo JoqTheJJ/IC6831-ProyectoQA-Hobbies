67135c4949ba71f66bd1093da5614e699737bfc7
JoqTheJJ <128062949+JoqTheJJ@users.noreply.github.com>
2025-06-07 02:04:06 -0600

    alt db

--- server/db/neo4j.js
+++ server/db/neo4j.js
@@ -1,11 +1,18 @@
 const neo4j = require('neo4j-driver');
 
+/*
 const config = {
   uri: 'bolt://34.238.135.120',
   user: 'BetaDBMS',
   password: 'betabeta2'
 };
+*/
 
+const config = {
+  uri: 'bolt://3.231.165.171',
+  user: 'neo4j',
+  password: 'thermometer-traffic-navigators'
+};
 
 const neo4jClient = neo4j.driver(config.uri, neo4j.auth.basic(config.user, config.password));
 
